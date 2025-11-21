ee9cfa2206ad49ba7e11db9f350055e8fff9c268
Borislav Gavrilov <136329010+borislavGavrilov@users.noreply.github.com>
2025-11-21 14:29:30 +0200

    Add catalog and Home components + Routes

--- client/Games-Project/src/App.jsx
+++ client/Games-Project/src/App.jsx
@@ -1,7 +1,8 @@
-import Catalog from "./components/catalog";
+import Catalog from "./components/catalog/Catalog";
 import { Routes, Route, Link } from "react-router";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import Home from "./components/home/Home";
 
 function App() {
   return (
@@ -9,59 +10,10 @@ function App() {
       <>
         <Header />
 
-        {/* Home Page */}
-        <section id="welcome-world">
-          <div className="welcome-message">
-            <h2>ALL new games are</h2>
-            <h3>Only in </h3>
-            <img id="logo-left" src="./images/logo.png" alt="logo" />
-          </div>
-
-          <div id="home-page">
-            <h1>Latest Games</h1>
-
-            <div id="latest-wrap">
-              {/* Display div: with information about every game (if any) */}
-              <div className="home-container">
-                <div className="game">
-                  <img src="./images/witcher.png" alt="The Witcher 3" />
-                  <div className="details-overlay">
-                    <p className="name">The Witcher 3</p>
-                    <p className="genre">Open World</p>
-                    <Link to="/details/witcher" className="details-button">
-                      Details
-                    </Link>
-                  </div>
-                </div>
-
-                <div className="game">
-                  <img src="./images/elden ring.png" alt="Elden Ring" />
-                  <div className="details-overlay">
-                    <p className="name">Elden Ring</p>
-                    <p className="genre">Action RPG</p>
-                    <Link to="/details/elden-ring" className="details-button">
-                      Details
-                    </Link>
-                  </div>
-                </div>
-
-                <div className="game">
-                  <img src="./images/minecraft.png" alt="Minecraft" />
-                  <div className="details-overlay">
-                    <p className="name">Minecraft</p>
-                    <p className="genre">Sandbox</p>
-                    <Link to="/details/minecraft" className="details-button">
-                      Details
-                    </Link>
-                  </div>
-                </div>
-
-                {/* Display paragraph: If there are no games */}
-                {/* <p className="no-articles">No games yet</p> */}
-              </div>
-            </div>
-          </div>
-        </section>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+        </Routes>
 
         <Footer />
       </>
